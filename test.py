1ecefa614e57dbde6bf2709ec9604a1ebfc2906a
Joe Cui <xiaozhou.cui@gmail.com>
2022-03-27 17:45:39 +1000

    Initial commit

---
+++ test.py
@@ -0,0 +1,36 @@
+import csv
+
+new_data = []
+mapper = []
+result = []
+
+with open('./csv/new_sql_out.csv', 'r') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        new_data.append(row)
+
+with open('./csv/mapper.csv', 'r') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        mapper.append(row)
+
+existing_ids = [d['userId'] for d in mapper]
+new_ids = [d['user_id'] for d in new_data]
+missing_ids = list(filter(lambda id: id not in existing_ids, new_ids))
+
+for dict in mapper:
+    if dict['userId'] in new_ids:
+        item = {'internalId': dict['userId'],
+                'externalId': dict['externalId'], 'createdAt': dict['createdAt']}
+        result.append(item)
+
+
+with open('./csv/result.csv', 'w', newline='') as csvfile:
+    fieldnames = ['internalId', 'externalId', 'createdAt']
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    writer.writeheader()
+    for dict in result:
+        writer.writerow({'internalId': dict['internalId'],
+                         'externalId': dict['externalId'], 'createdAt': dict['createdAt']})
+
+print(missing_ids)
