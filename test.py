2b0a5c86263319dde1f7337fc965442fd7056fa8
Joe Cui <xiaozhou.cui@gmail.com>
2022-03-28 15:46:05 +1000

    Include removed values

--- test.py
+++ test.py
@@ -16,7 +16,8 @@ with open('./csv/mapper.csv', 'r') as csvfile:
 
 existing_ids = [d['userId'] for d in mapper]
 new_ids = [d['user_id'] for d in new_data]
-missing_ids = list(filter(lambda id: id not in existing_ids, new_ids))
+added_ids = list(filter(lambda id: id not in existing_ids, new_ids))
+removed_ids = list(filter(lambda id: id not in new_ids, existing_ids))
 
 for dict in mapper:
     if dict['userId'] in new_ids:
@@ -27,11 +28,12 @@ for dict in mapper:
 sorted_result = sorted(result, key=lambda d: d['createdAt'])
 
 with open('./csv/result.csv', 'w', newline='') as csvfile:
-    fieldnames = ['internalId', 'externalId', 'createdAt']
+    fieldnames = ['createdAt', 'externalId', 'internalId']
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
     writer.writeheader()
     for dict in sorted_result:
-        writer.writerow({'internalId': dict['internalId'],
-                         'externalId': dict['externalId'], 'createdAt': dict['createdAt']})
+        writer.writerow({'createdAt': dict['createdAt'],
+                         'externalId': dict['externalId'], 'internalId': dict['internalId']})
 
-print(missing_ids)
+for id in removed_ids:
+    print(id)
