714b99ac824ff185e647546472e7113b9eb64cb6
Joe Cui <xiaozhou.cui@gmail.com>
2022-05-16 11:06:30 +1000

    Update merge script

--- merge.py
+++ merge.py
@@ -2,9 +2,9 @@ import csv
 
 merged = []
 
-for num in range(1, 4):
+for num in range(1, 5):
     print(f'Loading results({str(num)}).csv...')
-    with open(f'./csv/results({str(num)}).csv') as file:
+    with open(f'./csv/results ({str(num)}).csv') as file:
         reader = csv.DictReader(file)
         for row in reader:
             merged.append(row)
@@ -12,11 +12,11 @@ for num in range(1, 4):
 sorted_result = sorted(merged, key=lambda d: d['createdAt'])
 
 with open('./csv/merged.csv', 'w', newline='') as file:
-    fieldnames = ['createdAt', 'externalId', 'internalId']
+    fieldnames = ['createdAt', 'externalId', 'userId']
     writer = csv.DictWriter(file, fieldnames=fieldnames)
     writer.writeheader()
     for dict in sorted_result:
         writer.writerow({'createdAt': dict['createdAt'],
-                         'externalId': dict['externalId'], 'internalId': dict['internalId']})
+                         'externalId': dict['externalId'], 'userId': dict['userId']})
 
 print('Created merged.csv')
